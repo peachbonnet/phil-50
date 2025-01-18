bd015e185ab23f5aaf4d4b547b16844536d29242
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2025-01-18 11:40:43 -0500

    add: progress to 3

--- src/sections/ProgressBar.jsx
+++ src/sections/ProgressBar.jsx
@@ -2,7 +2,7 @@ import ProgressLine from "../components/ProgressLine";
 import "./ProgressBar.css";
 
 export default function ProgressBar() {
-  const amountOfSponsors = 2;
+  const amountOfSponsors = 3;
   const percentage = ((amountOfSponsors / 50) * 100).toString() + "%";
   return (
     <section className="progress-bar">
